30f972a6fadb88f6f057b91a6d871fb2bab57379
resilient-coders36 <resilient-coders36@resilient-new3s-MacBook-Pro.local>
2020-02-15 22:37:28 -0500

    first codewars

---
+++ question1_PaperWork.js
@@ -0,0 +1,21 @@
+// PROBLEM Beginner Series #1 School Paperwork //
+
+// Your classmates asked you to copy some paperwork for them.
+// You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your task is to calculate how many blank pages do you need.
+// Example:
+// paperwork(5, 5) == 25
+// Note! if n or m < 0 return 0! Waiting for translations and Feedback! Thanks!
+
+//ANSWER//
+let classmates = 0;
+let pages = 0;
+function paperwork(classmates, pages){
+  let totalPages = classmates * pages
+  if (classmates < 0 || pages < 0){
+    return (0);
+  }
+  else{
+    return totalPages
+  }
+}
---
+++ question3_oppositeNumber.js
@@ -0,0 +1,6 @@
+// Very simple, given a number, find its opposite.
+
+// ANSWER // 
+function opposite(number) {
+  return(-number)
+}
