e4c8959bc117009ac8ad51f078b76642a6fd5df1
resilient-coders36 <resilient-coders36@resilient-new3s-MacBook-Pro.local>
2020-03-07 10:27:50 -0500

    work already

--- DueFeb16/question3_oppositeNumber.js
+++
@@ -1,8 +0,0 @@
-// Very simple, given a number, find its opposite.
-
-// ANSWER //
-function opposite(number) {
-  return(-number)
-}
-
-console.log( 'test question three answer:' + ' '+ opposite(5))
