d35399a8aff0154672a192328feba9a91f63b09e
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-03-18 18:48:15 -0400

    adding March 18th

---
+++ march/march18/main.js
@@ -0,0 +1,10 @@
+function breakChocolate(n,m) {
+  let area = n*m
+  if(area === 0){
+  return 0;
+  }
+  else{return area-1}
+
+}
+
+console.log('sample answer for March 18th is: ' + ' ' + '' + breakChocolate(10,10));
