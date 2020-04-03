ce3c2afc72feb799bae5d0f93f8072cf388e8aa5
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-04-02 22:24:58 -0400

    adding April 2nd

--- march/march31_leetcode/main.js
+++ march/march31_leetcode/main.js
@@ -1,13 +1,8 @@
 // Kenneth cruz
 // Leetcode Level : Easy
-// Leetcode problem: Two Sum
+// Leetcode problem: Defanging an IP Address
 
-var twoSum = function(nums, target) {
-    for(i=0;i<nums.length;i++){
-      for (j = i + 1; j < nums.length; j++){
-          if ( target - nums[i] == nums[j]){
-              return [i, j]
-          }
-      }
-    }
-}
+var defangIPaddr = function(address) {
+    let newAddress = address.split('.').join('[.]')
+    return newAddress;
+};
