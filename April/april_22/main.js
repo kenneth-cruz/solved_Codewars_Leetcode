035909a537897ffdcd39c3a486d72d23dbf8788e
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-04-22 09:49:27 -0400

    adding april 22nd

---
+++ April/april_22/main.js
@@ -0,0 +1,26 @@
+// Kenneth cruz ... April 22nd, 2020
+// CodeWar Level : 7 KYU
+// Leetcode problem: X's and O's
+
+function XO(str) {
+    console.log(str, 'hello')
+    let xcounter = 0;
+    let ocounter = 0;
+    for(let i = 0 ; i < str.length ; i++){
+      if(str[i].toLowerCase() === "x"){
+        xcounter ++;
+      }
+      if(str[i].toLowerCase() === "o"){
+        ocounter ++;
+      }
+      else{xcounter += 0; ocounter += 0;}
+
+    }
+    if(xcounter === ocounter){
+      return true;
+    }
+    if(xcounter === undefined || ocounter === undefined){
+     return false;
+    }
+    else{return false}
+}
