bf3a99462f6207949a2d1881368d176ee41cf4e8
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-05-03 15:18:57 -0400

    adding may 3rd

---
+++ May/may_03(L)/main.js
@@ -0,0 +1,17 @@
+// Kenneth cruz ... May 2nd, 2020
+// LeetCode Level : Easy
+// LeetCode problem: Kids With the Greatest Number of Candies
+
+var kidsWithCandies = function(candies, extraCandies) {
+    let bigCandy = [];
+    let resultArray= [];
+    let highestCandy = Math.max(...candies)
+    candies.forEach(candy => bigCandy.push(candy += extraCandies));
+    for(let i = 0; i < bigCandy.length; i++){
+        if(bigCandy[i] < highestCandy){
+            resultArray.push(false);
+        }
+        else{resultArray.push(true);}
+    }
+    return(resultArray)
+};
