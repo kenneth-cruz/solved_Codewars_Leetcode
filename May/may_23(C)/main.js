481bde2112536f2cf438fae38bae59d70d13780b
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2020-05-23 10:49:44 -0400

    adding may 23rd

---
+++ May/may_23(C)/main.js
@@ -0,0 +1,7 @@
+// Kenneth cruz ... May 23rd, 2020
+// Codewar Level : 7Kyu
+// Codewar problem: Greet Me
+
+var greet = function(name) {
+  return`Hello ${name[0].toUpperCase()}${name.substring(1,name.length).toLowerCase()}!`
+};
