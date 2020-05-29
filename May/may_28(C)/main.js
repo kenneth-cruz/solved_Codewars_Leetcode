bf9bec5f63e4cd5b0d28c995881be36968d5c24e
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2020-05-28 23:57:05 -0400

    adding may 28

---
+++ May/may_28(C)/main.js
@@ -0,0 +1,29 @@
+// Date: May 28th, 2020
+// codewar Level: 8
+
+
+
+
+function greet(lang) {
+    return langs[lang]||langs['english'];
+  }
+  
+  var langs = {
+  'english': 'Welcome',
+  'czech': 'Vitejte',
+  'danish': 'Velkomst',
+  'dutch': 'Welkom',
+  'estonian': 'Tere tulemast',
+  'finnish': 'Tervetuloa',
+  'flemish': 'Welgekomen',
+  'french': 'Bienvenue',
+  'german': 'Willkommen',
+  'irish': 'Failte',
+  'italian': 'Benvenuto',
+  'latvian': 'Gaidits',
+  'lithuanian': 'Laukiamas',
+  'polish': 'Witamy',
+  'spanish': 'Bienvenido',
+  'swedish': 'Valkommen',
+  'welsh': 'Croeso'
+  };
